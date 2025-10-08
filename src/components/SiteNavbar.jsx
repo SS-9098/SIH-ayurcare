6dc51bd16e2957ad78247692ccae2fac5f5dae2b
shivika singh <shivika27may@gmail.com>
2025-10-09 00:15:02 +0530

    added ne folder inside src and created new file

--- src/components/SiteNavbar.jsx
+++ src/components/SiteNavbar.jsx
@@ -3,7 +3,7 @@ import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 import { FaLeaf, FaUserMd } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../App';
-import '../App.css'; 
+import './App.css';
 
 const SiteNavbar = () => {
   const { authState, setAuthState } = useContext(AuthContext);
@@ -19,19 +19,20 @@ const SiteNavbar = () => {
       <Container>
         <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
           <FaLeaf size={28} className="me-2 text-success" />
-          <span className="fw-bold ayurvedic-brand-name">Ayurveda Wellness</span>
+          {/* Changed Name */}
+          <span className="fw-bold ayurvedic-brand-name">AyushAhar</span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link as={Link} to="/" className="ayurvedic-nav-link">Home</Nav.Link>
             <Nav.Link as={Link} to="/about" className="ayurvedic-nav-link">About</Nav.Link>
-            <Nav.Link as={Link} to="/assessment" className="ayurvedic-nav-link">Dosha Assessment</Nav.Link>
             {!authState.isAuthenticated && (
               <Nav.Link as={Link} to="/doctor-login" className="ayurvedic-nav-link">Login</Nav.Link>
             )}
             {authState.isAuthenticated && (
               <>
+                <Nav.Link as={Link} to="/assessment" className="ayurvedic-nav-link">Dosha Assessment</Nav.Link>
                 <Nav.Link as={Link} to="/dashboard" className="ayurvedic-nav-link">Dashboard</Nav.Link>
                 <Button variant="outline-success" className="ms-2 ayurvedic-nav-btn" onClick={handleLogout}>
                   Logout
