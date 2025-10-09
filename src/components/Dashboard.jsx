5a15d432d0b6a40e9520145770f4d122f17dbe11
Shreyans <shreyanssaraf10@gmail.com>
2025-10-09 11:12:31 +0530

    Revert "Fix import path for App.css in Dashboard component"
    This reverts commit 1fd3b1cb3d47bc3e452b34584d834478ded37fb4.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,7 +3,7 @@ import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import { FaUserMd, FaUser, FaSignOutAlt, FaLeaf } from 'react-icons/fa';
 import { AuthContext } from '../App';
 import { useNavigate } from 'react-router-dom';
-import '../App.css'; // For general styling
+import './App.css'; // For general styling
 
 const Dashboard = () => {
     const { authState, setAuthState } = useContext(AuthContext);
